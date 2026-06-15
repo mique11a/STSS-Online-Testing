e312413b562770fddea99d848c6f3ba7251931bc
HongseFox <1878873899@qq.com>
2026-06-15 19:01:38 +0800

    Revert "feat: add validEndTime to begin_an_exam for time window clamping"
    This reverts commit d157bd4fbbe7d8f5d9d72b8b4db1a89f9e2d3627.

--- java/src/main/java/com/stss/online_testing/core/proctor/ProctorFacade.java
+++ java/src/main/java/com/stss/online_testing/core/proctor/ProctorFacade.java
@@ -117,7 +117,6 @@ public class ProctorFacade {
         Map<String, Object> data = new LinkedHashMap<>();
         data.put("recordId", draft.getId());
         data.put("startedAt", draft.getCreateTime());
-        data.put("validEndTime", examPaper.getValidEndTime());
         data.put("attemptsUsed", submittedCount);
         data.put("remainingAttempts", Math.max(config.getAllowedAttempts() - submittedCount, 0));
         data.put("paper", paper);
